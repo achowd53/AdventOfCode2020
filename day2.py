213c0922f427d585e219c8be8a62daea61192689
Arib <aribc@mit.edu>
2020-12-02 00:26:37 -0500

    .count() exists reeeeee

--- day2.py
+++ day2.py
@@ -3,20 +3,12 @@ o2=0
 l=[] 
 for s in open("day2.txt"):
     s=s.strip()
-    ran,letter,password = s.split(" ")
+    ran,c,passw = s.split(" ")
     a,b = map(int,ran.split("-"))
-    letter = letter[0]
-    c=0
-    for x in password:
-        if x==letter:
-            c+=1
-    if a<=c<=b:
+    c = c[0]
+    if a<=passw.count(c)<=b:
         o1+=1
-    if password[a-1] == letter:
-        if password[b-1] != letter:
-            o2+=1
-    if password[b-1] == letter:
-        if password[a-1] != letter:
-            o2+=1
+    if (passw[a-1]+passw[b-1]).count(c)==1:
+        o2+=1
 print(o1)
 print(o2)

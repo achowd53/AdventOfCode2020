46dba92dede6c09766f6e5738e6be733fe1df393
Arib <aribc@mit.edu>
2020-12-02 00:39:10 -0500

    More quick edits on day2

--- day2.py
+++ day2.py
@@ -2,12 +2,12 @@ o1=0
 o2=0
 for s in open("day2.txt"):
     s=s.strip()
-    ran,c,passw = s.split(" ")
+    ran,c,pas = s.split()
     a,b = map(int,ran.split("-"))
     c = c[0]
-    if a<=passw.count(c)<=b:
+    if a<=pas.count(c)<=b:
         o1+=1
-    if (passw[a-1]+passw[b-1]).count(c)==1:
+    if (pas[a-1]+pas[b-1]).count(c)==1:
         o2+=1
 print(o1)
 print(o2)

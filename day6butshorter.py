4f0918b9541b9d729ac9e9d958f9aea7a6b87113
Arib <aribc@mit.edu>
2020-12-06 14:29:59 -0500

    Cleaning!

--- day6butshorter.py
+++
@@ -1,3 +0,0 @@
-day=open("day6.txt").read().split('\n\n')
-print("Part 1:",sum(len(set(line)-{'\n'}) for line in day))
-print("Part 2:",sum(len(set.intersection(*map(set,line.split("\n")))) for line in day))

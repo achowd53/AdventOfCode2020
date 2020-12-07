21005f80063d456e55c3869e29a3b7809574fc84
Arib <aribc@mit.edu>
2020-12-07 01:17:26 -0500

    Nothing notable here

--- day7/day7.py
+++ day7/day7.py
@@ -1,21 +1,26 @@
-import re #Flashback to regex, re.fullmatch(r'#[0-9a-f]{6}',hcl) and re.fullmatch(r'[0-9]{9}',pid)
-import string
-#Sets are POG {}, A|B finds union of sets, A&B finds intersection, A-B finds difference, A^B is (A|B)-(A&B)
-#Flexing sets with a day6 code reminder:
-    #day=open("day6.txt").read().split('\n\n')
-    #print("Part 1:",sum(len(set(line)-{'\n'}) for line in day))
-    #print("Part 2:",sum(len(set.intersection(*map(set,line.split("\n")))) for line in day))
+import collections
+import re
 
-part1=part2=0
+lines = [line.strip() for line in open("day7.txt").readlines()]
+contains = collections.defaultdict(list)
+inside = collections.defaultdict(set)
+for line in lines:
+    color = line.split(" bags contain ")[0]
+    contains[color] = []
+    if line.split(" contain ")[1] == "no other bags.":
+        continue
+    for value,inner in re.findall(r'(\d+) (.+?) bags?[,.]', line):
+        contains[color].append((int(value), inner))
+        inside[inner].add(color)
 
-c=0
-l=[]
-#for s in open("day7.txt"):
-#    s=s.strip()
-    
-for line in open("day7.txt").read().split('\n\n'):
-    pass
+contains_gold = set()
+def what_has(color):
+    for color in inside[color]:
+        contains_gold.add(color)
+        what_has(color)
+what_has("shiny gold")
+print("Part 1:",len(contains_gold))
 
-print(part1)
-print(part2)
-
+def bags(color):
+    return sum(value * (1+bags(inner)) for value,inner in contains[color])
+print("Part 2:",bags("shiny gold"))

f821d7e24d8e9af66f0305d5d792315395a0676b
Arib <aribc@mit.edu>
2020-12-07 01:30:14 -0500

    Base day template

---
+++ template.py
@@ -0,0 +1,15 @@
+import re #re.split(), re.match, re.findall, Regex Guide at https://www.debuggex.com/cheatsheet/regex/python
+import collections #defaultdicts, maybe counters
+import string #ascii_uppercase, ascii_lowercase
+#Sets are POG {}, A|B finds union of sets, A&B finds intersection, A-B finds difference, A^B is (A|B)-(A&B)
+
+part1 = part2 = 0
+#lines = open("day8.txt").read().split('\n\n')
+#lines = [line.strip() for line in open("day8.txt").readlines()]
+#lines = open("day8.txt")
+for line in lines:
+    pass
+
+
+print(part1)
+print(part2)

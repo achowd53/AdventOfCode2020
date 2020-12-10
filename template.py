94f9f4aac143d7b066d305cd801e9d35bc1a49ee
Arib <aribc@mit.edu>
2020-12-10 00:57:33 -0500

    Day 10 complete, I did terrible part 1 and decent on part 2

--- template.py
+++ template.py
@@ -1,5 +1,6 @@
 import re #re.split(), re.match, re.findall, Regex Guide at https://www.debuggex.com/cheatsheet/regex/python
 import collections #defaultdicts, maybe counters
+import functools #use @lru_cache above a function to keep track of all inputs and speed it up
 import string, numpy, itertools
 #Sets are POG {}, A|B finds union of sets, A&B finds intersection, A-B finds difference, A^B is (A|B)-(A&B)
 
@@ -8,12 +9,12 @@ def run(filename):
 
     #lines = open(filename)
     #lines = open(filename).read().split('\n\n')
-    lines = open(filename).read().splitlines()
-    
+    lines = open(filename).readlines()
+
     for line in lines:
         pass
 
 
 run("example.txt")
 print()
-run("day9.txt")
+run("day11.txt")

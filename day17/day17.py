3dc4e7f785afc6507c67eb16a060a114e96eab8d
Arib <aribc@mit.edu>
2020-12-17 01:33:16 -0500

    Made day 17 slightly harder to read yeah...

--- day17/day17.py
+++ day17/day17.py
@@ -4,10 +4,10 @@ cubes_p1={(x,y,0)for(y,line),x in product(enumerate(lines),range(len(lines[0])))
 cubes_p2={(x,y,0,0)for(y,line),x in product(enumerate(lines),range(len(lines[0])))if line[x]=='#'}
 for _ in range(6):
     adj_p1,adj_p2 = defaultdict(int),defaultdict(int)
-    for (x,y,z),a,b,c in product(cubes_p1,[-1,0,1],[-1,0,1],[-1,0,1]):
+    for(x,y,z),a,b,c in product(cubes_p1,[-1,0,1],[-1,0,1],[-1,0,1]):
         if any([a!=b,b!=c,c!=0]): adj_p1[(x+a,y+b,z+c)]+=1
-    for (x,y,z,w),a,b,c,d in product(cubes_p2,[-1,0,1],[-1,0,1],[-1,0,1],[-1,0,1]):
+    for(x,y,z,w),a,b,c,d in product(cubes_p2,[-1,0,1],[-1,0,1],[-1,0,1],[-1,0,1]):
         if any([a!=b,b!=c,c!=d,d!=0]): adj_p2[(x+a,y+b,z+c,w+d)]+=1
-    cubes_p1 = {cube for cube in cubes_p1 if adj_p1[cube]in[2,3]}|{cube for cube in adj_p1 if adj_p1[cube]==3}
-    cubes_p2 = {cube for cube in cubes_p2 if adj_p2[cube]in[2,3]}|{cube for cube in adj_p2 if adj_p2[cube]==3}
+    cubes_p1={i for i in cubes_p1 if adj_p1[i]in[2,3]}|{i for i in adj_p1 if adj_p1[i]==3}
+    cubes_p2={i for i in cubes_p2 if adj_p2[i]in[2,3]}|{i for i in adj_p2 if adj_p2[i]==3}
 print("Part 1:",len(cubes_p1),"\nPart 2:",len(cubes_p2))

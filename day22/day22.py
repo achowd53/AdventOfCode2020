d012e6d6f2e459555412ee2f9813f9b764df68e3
Arib <aribc@mit.edu>
2020-12-22 01:17:53 -0500

    Fixed some spacings

--- day22/day22.py
+++ day22/day22.py
@@ -3,7 +3,7 @@ play1,play2 = [[int(c) for c in p.splitlines()[1:]]for p in open("day22.txt").re
 p1_play1,p2_play1,p1_play2,p2_play2 = deque(play1), deque(play1), deque(play2), deque(play2)
 while p1_play1 and p1_play2:
     c1,c2 = p1_play1.popleft(), p1_play2.popleft()
-    if c1 > c2: p1_play1.extend([c1,c2])
+    if c1>c2: p1_play1.extend([c1,c2])
     else: p1_play2.extend([c2,c1])
 print("Part 1:",sum(i*v for i,v in enumerate(list(p1_play1)if p1_play1 else reversed(p1_play2),1)))
 def RC(p1,p2):
@@ -14,7 +14,7 @@ def RC(p1,p2):
         c1,c2 = p1.popleft(), p2.popleft()
         if len(p1)>=c1 and len(p2)>=c2: win,_ = RC(deque(islice(p1,c1)),deque(islice(p2,c2)))
         else: win = "player1" if c1>c2 else "player2"
-        if win=="player1":p1.extend([c1,c2])
-        else:p2.extend([c2,c1])
+        if win=="player1": p1.extend([c1,c2])
+        else: p2.extend([c2,c1])
     return(win,p1 if win=="player1"else p2)
-print("Part 2:",sum(i * v for i, v in enumerate(reversed(RC(p2_play1,p2_play2)[1]), 1)))
+print("Part 2:",sum(i*v for i,v in enumerate(reversed(RC(p2_play1,p2_play2)[1]),1)))
